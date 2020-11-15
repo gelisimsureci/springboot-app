d9f3c2426c926cbab66cb0c95edeeba3b04aa9cd
Muhammet Murat Gülcü <mgulcu@btctr.local>
2020-11-15 22:38:25 +0300

    mgulcu first commit

--- src/main/java/com/springboot/app/controller/EmployeeController.java
+++ src/main/java/com/springboot/app/controller/EmployeeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloController {
+public class EmployeeController {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/hello")
+	@RequestMapping(method = RequestMethod.GET, value = "/employees")
 	public String hello() {
-		return "hello";
+		return "index";
 	}
 }
